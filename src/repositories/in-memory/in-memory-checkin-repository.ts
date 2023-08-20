1a422cbf0e9d9074919569a77b08781c1b0763ea
luiz <jose.iscosistemas@gmail.com>
2023-08-20 11:15:21 -0700

    Validando o test checkins 2x no dia

--- src/repositories/in-memory/in-memory-checkin-repository.ts
+++ src/repositories/in-memory/in-memory-checkin-repository.ts
@@ -1,7 +1,7 @@
 import { CheckIn, Prisma } from '@prisma/client'
 import { ICheckInRepository } from '../check-in-repository'
 import crypto from 'node:crypto'
-
+import dayjs from 'dayjs'
 export class InMemoryCheckInRepository implements ICheckInRepository {
   public items: CheckIn[] = []
 
@@ -22,9 +22,16 @@ export class InMemoryCheckInRepository implements ICheckInRepository {
     userId: string,
     date: Date,
   ): Promise<CheckIn | null> {
-    const checkInSameday = this.items.find(
-      (checkIn) => checkIn.user_id === userId && checkIn.created_at === date,
-    )
+    const startOfTheDay = dayjs(date).startOf('date')
+    const endOfTheDay = dayjs(date).endOf('date')
+
+    const checkInSameday = this.items.find((checkIn) => {
+      const checkInDate = dayjs(checkIn.created_at)
+      const isOnSameDate =
+        checkInDate.isAfter(startOfTheDay) && checkInDate.isBefore(endOfTheDay)
+
+      return checkIn.user_id === userId && isOnSameDate
+    })
 
     if (!checkInSameday) {
       return null
