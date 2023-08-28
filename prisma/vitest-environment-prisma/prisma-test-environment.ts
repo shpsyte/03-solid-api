a565b0c983c427012a7d8d3e82d6e68cc84fd1db
luiz <jose.iscosistemas@gmail.com>
2023-08-28 14:15:10 -0700

    Test Environment do Prisma

--- prisma/vitest-environment-prisma/prisma-test-environment.ts
+++ prisma/vitest-environment-prisma/prisma-test-environment.ts
@@ -1,16 +1,45 @@
+import 'dotenv/config'
+import { randomUUID } from 'node:crypto'
+import { execSync } from 'node:child_process'
 import { Environment } from 'vitest'
+import { PrismaClient } from '@prisma/client'
 
 // vitest-environment-prisma
+
+// postgresql://docker:docker@localhost:5432/apisolid?schema=public
+const prisma = new PrismaClient()
+
+function generateSchema(schema: string) {
+  if (!process.env.DATABASE_URL) {
+    throw new Error('DATABASE_URL is not set.')
+  }
+
+  const url = new URL(process.env.DATABASE_URL)
+
+  url.searchParams.set('schema', schema)
+
+  return url.toString()
+}
 
 export default <Environment>{
   name: 'prisma',
   transformMode: 'ssr',
   async setup() {
-    console.log('setup')
+    const schema = randomUUID()
+    const url = generateSchema(schema)
+
+    process.env.DATABASE_URL = url
+
+    // run prisma migrate
+    execSync('npx prisma migrate deploy')
 
     return {
       async teardown() {
-        console.log('teardown')
+        console.log('teardown db')
+        await prisma.$executeRawUnsafe(
+          `DROP SCHEMA IF EXISTS "${schema}" CASCADE`,
+        )
+        await prisma.$disconnect()
       },
     }
   },
---
+++ src/__test__/e2e/register2.spec.ts
@@ -0,0 +1,5 @@
+import { test, expect } from 'vitest'
+
+test('ok 2', () => {
+  expect(1).toBe(1)
+})
